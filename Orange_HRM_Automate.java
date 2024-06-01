8e457b163969fe7c16e694d6b46751ab08e4c0ce
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2024-06-02 00:43:57 +0530

    Orange_HRM_Automate

--- Orange_HRM_Automate.java
+++ Orange_HRM_Automate.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Orange_HRM_Automate {
-
+//Orange_HRM_Automate
 	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver", "G:\\New Selenium\\driver\\chromedriver.exe"); 
